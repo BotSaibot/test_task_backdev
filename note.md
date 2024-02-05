33517eb7a7be0f52881e854d7ca043e9d16974e2
Kira <bot.saibot@yandex.ru>
2024-02-05 19:33:27 +0300

    Added some structures, POST method support and debug router setup

--- note.md
+++ note.md
@@ -1,2 +1,4 @@
 https://wp-kama.ru/handbook/rest/basic#routes-endpoints - REST маршрута
+https://codex.so/go-web-server - Простейший веб-сайт на Go
+
 
--- web-hello-world.go
+++ web-hello-world.go
@@ -7,36 +7,70 @@ import (
 	"net/http"
 )
 
-func main() {
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	// })
+type SingIn struct {
+	GUID string `json:"guid"`
+}
 
-	// http.ListenAndServe(":80", nil)
+func main() {
 	servMux := http.NewServeMux()
 
 	servMux.HandleFunc("/route1/", route1Handler)
 	servMux.HandleFunc("/route2/", route2Handler)
+	servMux.HandleFunc("/debug/", debugHandler)
 
 	log.Fatal(http.ListenAndServe(":8941", servMux))
 }
 
-type reqMsg struct {
-	Msg  string `json:"msg"`
-	Par0 string `json:"parameter"`
-}
+func route1Handler(w http.ResponseWriter, req *http.Request) {
+	// Первый маршрут выдает пару Access, Refresh токенов для пользователя сидентификатором (GUID) указанным в параметре запроса
+	type reqMsg struct {
+		AccessToken  string `json:"accessToken"`
+		RefreshToken string `json:"refreshToken"`
+		DebugGUID    string `json:"debug-guid"`
+	}
 
-func route1Handler(wrt http.ResponseWriter, req *http.Request) {
-	msg := fmt.Sprintf("Route 1:\nHello, you've requested: %s\n", req.URL.Path)
-	js, err := json.MarshalIndent(reqMsg{Msg: msg, Par0: "v any"}, "", "\t")
-	if err != nil {
-		http.Error(wrt, err.Error(), http.StatusInternalServerError)
+	if req.Method == http.MethodPost {
+		formVar := req.FormValue("guid")
+		js, err := json.MarshalIndent(reqMsg{
+			AccessToken:  "someAccessToken",
+			RefreshToken: "RefreshToken",
+			DebugGUID:    formVar,
+		}, "", "\t")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	} else {
+		http.Error(w, "404 page not found", http.StatusNotFound)
 		return
 	}
-	wrt.Header().Set("Content-Type", "application/json")
-	wrt.Write(js)
 }
 
-func route2Handler(wrt http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(wrt, "Route 2:\nHello, you've requested: %s\n", req.URL.Path)
+func route2Handler(w http.ResponseWriter, req *http.Request) {
+	// Второй маршрут выполняет Refresh операцию на пару Access, Refreshтокенов
+	fmt.Fprintf(w, "Route 2:\nHello, you've requested: %s\n", req.URL.Path)
+}
+
+func debugHandler(w http.ResponseWriter, req *http.Request) {
+	debPage := `<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+	<h1>Debug Route</h1>
+	<p>Вот два маршрута: <a href="/route1/">1 маршрут</a>; <a href="/route2/">2 маршрут</a></p>
+	<form action="/route1/" method="post">
+		<p>GUID: <input type="text" name="guid"></p>
+		<p><input type="submit"></p>
+	</form>
+</body>
+</html>`
+
+	fmt.Fprintf(w, debPage)
 }
